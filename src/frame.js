4bf1b68a88dd8f4057be0d2ec7518b2a1163c707
Jonas Daniels <jonas.daniels@outlook.com>
2024-04-23 23:14:24 -0700

    enable EIP-6963 support

--- src/frame.js
+++ src/frame.js
@@ -48,4 +48,33 @@ if (mmAppear) {
   } catch (e) {
     console.error('Frame Error:', e)
   }
+
+  // implement support for EIP-6963
+  announceProvider({
+    info: {
+      name: 'Frame',
+      rdns: 'sh.frame',
+      uuid: window.crypto.randomUUID(),
+      icon: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGAAAABgCAYAAADimHc4AAACmklEQVR4nO3csWsTYRyH8afXcihiMxdBUZwKpbPOTiIitlIqKP4FIoLi5Cau4uDgKKgotovSOri24CKIUlyk4uQkIg5ihzpcDkO5ixrS3/cu7/cDIXB3cL/mIe8lkN7Y/OIFuvYDV4BzwFFgL83yGVgE1tWDDNNE93kaeAEcFs7yNweBVeAksCaeZWgyYBJYodkvfqmc9bh6kGHJgMvAIfUg/6GMcEw9yDBkFGt+23QolqPWvxMmKNb/NurQ3GvBK+ANcB/42O/AjD8XYhueE8B1YAO40e/ALGScdOXAbeBq3QEOEOMWcKBqhwPE2AMsVO1wgDgzVRsdIM6lqo0OIOYAYg4g5gBiDiDmAGIOIOYAYg4g5gBiDiDmAGIOIOYAYg4gFh1gChhr+OPIrv31FaIDfAk+3yA2I0/mJUjMAcQcQMwBxBxAzAHEHEDMAcQcQMwBxBxAzAHEHEAsA76ph0hZBlwEfqkHSVUGPKe4B4MjCJTXgGUcQaL3IrwMnAe2RLMkaeenoCWKCBak6mPos/Ap0jG3c4O/B8R6BJzt3eAAsXLgMT0RHCBeGeEMOIBKDjwBTjuATg48cACtjgOIOYCYA4g5gJgDiDmAmAOIOYCYA4g5gJgDiDmAmAOIRQfYF3y+QUxGniz61vU/gs/XeF6CxBxAzAHEHEDMAcQcQMwBxBxAzAHEHEDMAcQcQMwBxBxAzAHE6gKshk6RsLoAb0OnSFhdgIfAduQgqaoL8B64EzlIqvpdhK8B9/A7YVeNT8/M1u3bBlaAl8B4d9tUxFAJ2fqXX0W87j6aao7iP9Bz9SAD+DAK3wOWaO+tdp6OQgBo5/2ONoG7oxIA2nW/o0/AKeD7KAWAYjlaAL6qB6nwE3gH3ARmgQ2A3xUaRM/9+UexAAAAAElFTkSuQmCC'
+    },
+    provider: new FrameProvider(new Connection())
+  })
+}
+
+/**
+ * Announces an EIP-1193 Provider.
+ */
+export function announceProvider (
+  detail
+) {
+  const event = new CustomEvent(
+    'eip6963:announceProvider',
+    { detail: Object.freeze(detail) }
+  )
+
+  window.dispatchEvent(event)
+
+  const handler = () => window.dispatchEvent(event)
+  window.addEventListener('eip6963:requestProvider', handler)
+  return () => window.removeEventListener('eip6963:requestProvider', handler)
 }
